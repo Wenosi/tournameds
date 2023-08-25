6a3e4e82572827510749a7884ebafbbfa27d7d6b
Wenosi <hope.pw@bk.ru>
2023-08-25 18:34:36 +0300

    добавление директорий и файлов

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<script src="https://kit.fontawesome.com/4c1d7b2736.js" crossorigin="anonymous"></script>
+	<meta http-equiv="refresh" content="15">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<center>
+		<div>
+		<?
+		$star = "<i class=\"fa-regular fa-star\"></i>";
+		$fstar = "<i class=\"fa-solid fa-star\"></i>";
+		?>
+			<h2>MaestroRuss | <?echo $fstar.$fstar.$fstar?>
+			-
+			<?echo $fstar.$star.$star?> | Tatarin</h2>
+		
+		</div>
+	</center><!--
+	<center>
+		<div>
+		<?
+		$star = "<i class=\"fa-regular fa-star\"></i>";
+		$fstar = "<i class=\"fa-solid fa-star\"></i>";
+		?>
+			<h2>Lose: 2 | <?echo $star.' Игр всего: 3 '?>
+			<?echo $star?> | Win: 1  </h2>
+		
+		</div>
+	</center>-->
+</body>
+</html>
